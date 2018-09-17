6a43801df113f56e674f4ce39b5d19ddffc28f9e
Tyler Nguyen <tylernguyen@Tylers-MacBook-Air.local>
2018-09-17 17:29:12 -0500

    refactoring logic

--- src/Game.java
+++ src/Game.java
@@ -5,54 +5,45 @@ public class Game {
 
     public Game(){
     }
-    // public boolean checkForWinner(String[][] gb){
-    //     for(int i = 0; i < 3; i++){
-    //         if(){
 
-    //         }
-    //     }
-    //     return false;
-    // }
-
-    public boolean checkVictory(String gameBoard[][]){
-        boolean victory = false;
-            //Check diag
-            if (gameBoard[1][1].equals("x")){
-                if(gameBoard[0][0].equals("x")){
-                    if(gameBoard[2][2].equals("x")){
-                        victory = true;
-                        messageHandler.playerXWonMessage();
-                    }
-                }
-                else if(gameBoard[0][2].equals("x")){
-                    if(gameBoard[2][0].equals("x")){
-                       victory = true;
-                       messageHandler.playerXWonMessage();
-                    }
-                }
-            }
-
+    public void checkVictory(String gameBoard[][]){
             for(int i = 0; i < 2; i++){
                 if(gameBoard[i][0].equals(gameBoard[i][1]) && gameBoard[i][1].equals(gameBoard[i][2])){
                     if(gameBoard[i][0].equals("x")){
                         messageHandler.playerXWonMessage();
+                        break;
                     } else if (gameBoard[i][0].equals("o")){
                         messageHandler.playerYWonMessage();
+                        break;
                     }
-                    victory = true;
                 } else if(gameBoard[0][i].equals(gameBoard[1][i]) && gameBoard[2][i].equals(gameBoard[2][i])){
-                    victory = true;
+                    if(gameBoard[0][i].equals("x")){
+                        messageHandler.playerXWonMessage();
+                        break;
+                    } else if (gameBoard[0][i].equals("o")){
+                        messageHandler.playerYWonMessage();
+                        break;
+                    }
                 } else if(gameBoard[0][0].equals(gameBoard[1][1]) && gameBoard[0][0].equals(gameBoard[2][2])){
-                    victory = true;
+                    if(gameBoard[0][0].equals("x")){
+                        messageHandler.playerXWonMessage();
+                        break;
+                    } else if (gameBoard[0][0].equals("o")){
+                        messageHandler.playerYWonMessage();
+                        break;
+                    }
                 } else if(gameBoard[0][2].equals(gameBoard[1][1]) && gameBoard[0][2].equals(gameBoard[2][0])){
-                    victory = true;
+                    if(gameBoard[0][2].equals("x")){
+                        messageHandler.playerXWonMessage();
+                        break;
+                    } else if (gameBoard[0][2].equals("o")){
+                        messageHandler.playerYWonMessage();
+                        break;
+                    }
                 } else {
-                    victory = false;
+                    System.out.println("No winner yet");
+                    break;
                 }
             }
-        return victory;
-    }
-    public static void checkWhoWon(Int i){
-        
     }
 }
