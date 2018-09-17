35ebf53845452626f0aff6249d628813e69c88d9
Tyler Nguyen <tylernguyen@Tylers-MacBook-Air.local>
2018-09-17 14:25:09 -0500

    refactoring check winner logic

--- src/Game.java
+++ src/Game.java
@@ -2,154 +2,57 @@ import java.util.*;
 
 public class Game {
     private static String[][] gameBoard = new String[3][3];
-    private static int count = 0;
-   // private Random piecePlacement = new Random();
 
     public Game(){
-      //  gameBoard = gb;
     }
+    // public boolean checkForWinner(String[][] gb){
+    //     for(int i = 0; i < 3; i++){
+    //         if(){
 
-    public boolean checkVictory(){
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    public boolean checkVictory(String gameBoard[][]){
         boolean victory = false;
             //Check diag
-            if (gameBoard[1][1].equals("X")){
-                if(gameBoard[0][0].equals("X")){
-                    if(gameBoard[2][2].equals("X")){
+            if (gameBoard[1][1].equals("x")){
+                if(gameBoard[0][0].equals("x")){
+                    if(gameBoard[2][2].equals("x")){
                         victory = true;
+                        messageHandler.playerXWonMessage();
                     }
                 }
-                else if(gameBoard[0][2].equals("X")){
-                    if(gameBoard[2][0].equals("X")){
+                else if(gameBoard[0][2].equals("x")){
+                    if(gameBoard[2][0].equals("x")){
                        victory = true;
-                    }
-                }
-            }
-            
-            //check adjacency
-            if(r == 0){
-                c = 0;
-                if(gameBoard[r+1][c].equals("X")){
-                    if(gameBoard[r+2][c].equals("X")){
-                       victory = true;
-                    }
-                }
-                if(gameBoard[r][c+1].equals("X")){
-                    if(gameBoard[r][c+2].equals("X")){
-                       victory = true;
+                       messageHandler.playerXWonMessage();
                     }
                 }
             }
 
-            //check first row
-            if (r == 1){
-                c = 0;
-                if(gameBoard[r][c+1].equals("X")){
-                    if(gameBoard[r][c+2].equals("X")){
-                       victory = true;
+            for(int i = 0; i < 2; i++){
+                if(gameBoard[i][0].equals(gameBoard[i][1]) && gameBoard[i][1].equals(gameBoard[i][2])){
+                    if(gameBoard[i][0].equals("x")){
+                        messageHandler.playerXWonMessage();
+                    } else if (gameBoard[i][0].equals("o")){
+                        messageHandler.playerYWonMessage();
                     }
-                }
-                if(gameBoard[r][c+1].equals("X")){
-                    if(gameBoard[r][c+2].equals("X")){
-                       victory = true;
-                    }
-                }
-            }
-
-            //check second row
-            if(r == 2){
-                c = 0;
-                if(gameBoard[r][c+1].equals("X")){
-                    if(gameBoard[r][c+2].equals("X")){
-                       victory = true;
-                    }
+                    victory = true;
+                } else if(gameBoard[0][i].equals(gameBoard[1][i]) && gameBoard[2][i].equals(gameBoard[2][i])){
+                    victory = true;
+                } else if(gameBoard[0][0].equals(gameBoard[1][1]) && gameBoard[0][0].equals(gameBoard[2][2])){
+                    victory = true;
+                } else if(gameBoard[0][2].equals(gameBoard[1][1]) && gameBoard[0][2].equals(gameBoard[2][0])){
+                    victory = true;
+                } else {
+                    victory = false;
                 }
             }
-
-            //check first column
-            if (c == 1){
-                r = 0;
-                if(gameBoard[r+1][c].equals("X")){
-                    if(gameBoard[r+2][c].equals("X")){
-                       victory = true;
-                    }
-                }
-            }
-
-            //check second column
-            if (c == 2){
-                r = 0;
-                if(gameBoard[r+1][c].equals("X")){
-                    if(gameBoard[r+2][c].equals("X")){
-                       victory = true;
-                    }
-                }
-            }
-            //Check diag
-            if (gameBoard[1][1].equals("O")){
-                if(gameBoard[0][0].equals("O")){
-                    if(gameBoard[2][2].equals("O")){
-                       victory = true;
-                    }
-                }
-                else if(gameBoard[0][2].equals("O")){
-                    if(gameBoard[2][0].equals("O")){
-                       victory = true;
-                    }
-                }
-            }
-            
-            //check adjacency
-            if(r == 0){
-                c = 0;
-                if(gameBoard[r+1][c].equals("O")){
-                    if(gameBoard[r+2][c].equals("O")){
-                       victory = true;
-                    }
-                }
-                if(gameBoard[r][c+1].equals("O")){
-                    if(gameBoard[r][c+2].equals("O")){
-                       victory = true;
-                    }
-                }
-            }
-            if (r == 1){
-                c = 0;
-                if(gameBoard[r][c+1].equals("O")){
-                    if(gameBoard[r][c+2].equals("O")){
-                       victory = true;
-                    }
-                }
-                if(gameBoard[r][c+1].equals("O")){
-                    if(gameBoard[r][c+2].equals("O")){
-                       victory = true;
-                    }
-                }
-            }
-            if(r == 2){
-                c = 0;
-                if(gameBoard[r][c+1].equals("O")){
-                    if(gameBoard[r][c+2].equals("O")){
-                       victory = true;
-                    }
-                }
-            }
-            if (c == 1){
-                r = 0;
-                if(gameBoard[r+1][c].equals("O")){
-                    if(gameBoard[r+2][c].equals("O")){
-                       victory = true;
-                    }
-                }
-            }
-            if (c == 2){
-                r = 0;
-                if(gameBoard[r+1][c].equals("O")){
-                    if(gameBoard[r+2][c].equals("O")){
-                       victory = true;
-                    }
-                }
-            }
-
         return victory;
+    }
+    public static void checkWhoWon(Int i){
+        
     }
 }
