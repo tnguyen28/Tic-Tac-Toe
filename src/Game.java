780b20bd2cc6bbed7fd98154052637720574dfd8
Tyler Nguyen <tylernguyen@Tylers-MacBook-Air.local>
2018-09-18 22:09:45 -0500

    updated methods to fit logic

--- src/Game.java
+++ src/Game.java
@@ -5,51 +5,7 @@ public class Game {
 
     public Game(){
     }
-
-    // public void checkVictory(String gameBoard[][], JFrame frame){
-    //         for(int i = 0; i < 2; i++){
-    //             if(gameBoard[i][0].equals(gameBoard[i][1]) && gameBoard[i][1].equals(gameBoard[i][2])){
-    //                 if(gameBoard[i][0].equals("x")){
-    //                     messageHandler.playerXWonMessage(frame);
-    //                     break;
-    //                 } else if (gameBoard[i][0].equals("o")){
-    //                     messageHandler.playerYWonMessage(frame);
-    //                     break;
-                    
-    //                 }
-    //             } else if(gameBoard[0][i].equals(gameBoard[1][i]) && gameBoard[2][i].equals(gameBoard[2][i])){
-    //                 if(gameBoard[0][i].equals("x")){
-    //                     messageHandler.playerXWonMessage(frame);
-    //                     break;
-                   
-    //                 } else if (gameBoard[0][i].equals("o")){
-    //                     messageHandler.playerYWonMessage(frame);
-    //                     break;
-                        
-    //                 }
-    //             } else if(gameBoard[0][0].equals(gameBoard[1][1]) && gameBoard[0][0].equals(gameBoard[2][2])){
-    //                 if(gameBoard[0][0].equals("x")){
-    //                     messageHandler.playerXWonMessage(frame);
-    //                     break;
-                    
-    //                 } else if (gameBoard[0][0].equals("o")){
-    //                     messageHandler.playerYWonMessage(frame);
-    //                     break;
-                       
-    //                 }
-    //             } else if(gameBoard[0][2].equals(gameBoard[1][1]) && gameBoard[0][2].equals(gameBoard[2][0])){
-    //                 if(gameBoard[0][2].equals("x")){
-    //                     messageHandler.playerXWonMessage(frame);
-    //                     break;
-                   
-    //                 } else if (gameBoard[0][2].equals("o")){
-    //                     messageHandler.playerYWonMessage(frame);
-    //                     break;
-                      
-    //                 }
-    //             }
-    //         }
-    // }
+    
     public Boolean victor(String[][] board, JFrame frame){
         //Diagonal checks
         if(board[1][1].equals("x")){
